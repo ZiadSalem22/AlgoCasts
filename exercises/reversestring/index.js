73545abd28649475e2614c354ca8fe4821529410
ZiadSalem22 <ziyadsalem22@gmail.com>
2024-07-15 08:17:32 -0400

    - solved reverseString

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,40 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+// // solution 1 usign string functions 
+// function reverse(str) {
+//     return str.split("").reverse().join("");
+// }
+
+//using for loop
+// function reverse(str) {
+
+//     let reversedSring = '';
+
+//     for (i = str.length - 1; i >= 0; i--){
+//         reversedSring += str[i];
+//     }
+
+//     return reversedSring;
+
+// }
+
+// using spread [ES6]
+// function reverse(str) {
+//      let reversedSring = [...str].reverse().join("");
+//     return reversedSring;
+// }
+
+
+// using recursion 
+
+function reverse(str) {
+
+    if (str === "")
+        return "";
+    else return reverse(str.substr(1)) + str.charAt(0);
+}
+
+
 
 module.exports = reverse;
