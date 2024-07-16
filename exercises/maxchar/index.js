84615840eb6d5ae4dac12f0af1a21611d8599a2a
ZiadSalem22 <ziyadsalem22@gmail.com>
2024-07-16 09:49:12 -0400

    - solved maxChar

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,24 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
 
-module.exports = maxChar;
+    char = {};
+    let maxChar = str[0];
+
+    for (let i of str) {
+
+        if (char[i] === undefined) char[i] = 1;
+        else {
+
+            char[i]++;
+            if (char[i] > char[maxChar]) maxChar = i; 
+        }
+    }
+
+    return maxChar;
+
+}
+    maxChar("a");
+
+    module.exports = maxChar;
