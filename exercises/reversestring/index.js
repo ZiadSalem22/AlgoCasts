7ee1db58afe783bee678985d3baa2153a62db5ca
z.salem <z.salem@ggw.com>
2025-05-02 10:44:57 -0400

    - inital commit : reversestring sloved

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,25 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+
+    //Take 1: using slpit, reverse and join
+    // return str.split('').reverse().join('');
+
+
+
+    //Take 2: empty string then append to the left
+    //     let reverseStr = '';
+    //     for ( let char of str)
+    //     {
+    //         reverseStr = char + reverseStr
+    //     }
+    //     return reverseStr; 
+
+    //Take 3: array --> reduce --> string
+    return str.split('').reduce((reversed, char) => char + reversed, '')
+
+
+}
 
 module.exports = reverse;
